3ad9b621dc70da34bf1ba41762bdcfcbbeee3892
ericchase <github@eric.addymail.com>
2025-03-15 22:39:55 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/push.ts
+++ tools/push.ts
@@ -7,30 +7,26 @@ import { Step_Async } from 'tools/lib/steps/Steps-Async.js';
 const builder = new Builder();
 
 builder.setStartupSteps([
-  Step_Bun_Run({ cmd: ['bun', 'update', '--latest'] }),
-  Step_Format(),
+  Step_Bun_Run({ cmd: ['bun', 'update', '--latest'] }, 'quiet'),
+  Step_Format('quiet'),
   //
 ]);
+
+const project_paths = [
+  '../Project@Template',
+  '../Project@Library@Lint',
+  '../Templates/Discord Bot',
+  '../Templates/Website',
+  // '../(Private) Projects/@mason/prodbybluezzi-beat-store',
+  //
+];
 
 builder.setCleanupSteps([
   // Update Local Server Files
   Step_MirrorDirectory({ from: 'src/lib/ericchase/', to: 'server/src/lib/ericchase/', include_patterns: ['Platform/FilePath.ts', 'Utility/Console.ts', 'Utility/UpdateMarker.ts'] }),
   // Push Files
-  Step_Async([
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Template' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Library@Lint' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Discord Bot' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Website' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../(Private) Projects/@mason/prodbybluezzi-beat-store' }),
-  ]),
-  // Double Check
-  Step_Async([
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Template' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Library@Lint' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Discord Bot' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Website' }),
-    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../(Private) Projects/@mason/prodbybluezzi-beat-store' }),
-  ]),
+  Step_Async(project_paths.map((path) => Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: path }))),
+  Step_Async(project_paths.map((path) => Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: path }))),
 ]);
 
 await builder.start();
