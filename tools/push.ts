ee98f24fc68b40281f2b74ef3a6d99b22ff3d66c
ericchase <github@eric.addymail.com>
2025-03-15 22:29:44 -0500

    more bugs
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/push.ts
+++ tools/push.ts
@@ -2,6 +2,7 @@ import { Builder } from 'tools/lib/Builder.js';
 import { Step_Bun_Run } from 'tools/lib/steps/Bun-Run.js';
 import { Step_Format } from 'tools/lib/steps/FS-Format.js';
 import { Step_MirrorDirectory } from 'tools/lib/steps/FS-MirrorDirectory.js';
+import { Step_Async } from 'tools/lib/steps/Steps-Async.js';
 
 const builder = new Builder();
 
@@ -15,13 +16,21 @@ builder.setCleanupSteps([
   // Update Local Server Files
   Step_MirrorDirectory({ from: 'src/lib/ericchase/', to: 'server/src/lib/ericchase/', include_patterns: ['Platform/FilePath.ts', 'Utility/Console.ts', 'Utility/UpdateMarker.ts'] }),
   // Push Files
-  Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Template' }),
-  Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Library@Lint' }),
-  // Step_Project_PushLib('../(Private) Projects/@mason/prodbybluezzi-beat-store'),
-  // Step_Project_PushLib('../Project@Library@Lint'),
-  // Step_Project_PushLib('../Templates/Discord Bot'),
-  // Step_Project_PushLib('../Templates/Website'),
-  //
+  Step_Async([
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Template' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Library@Lint' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Discord Bot' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Website' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../(Private) Projects/@mason/prodbybluezzi-beat-store' }),
+  ]),
+  // Double Check
+  Step_Async([
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Template' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Project@Library@Lint' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Discord Bot' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../Templates/Website' }),
+    Step_Bun_Run({ cmd: ['bun', 'run', 'pull'], dir: '../(Private) Projects/@mason/prodbybluezzi-beat-store' }),
+  ]),
 ]);
 
 await builder.start();
