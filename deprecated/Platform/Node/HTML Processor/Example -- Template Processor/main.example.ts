dcd779fc6b343a918207ad98c0b08f336cfe95a2
ericchase <github@eric.addymail.com>
2025-03-24 22:31:32 -0500

    move deprecated to archive folder
    Signed-off-by: ericchase <github@eric.addymail.com>

--- deprecated/Platform/Node/HTML Processor/Example -- Template Processor/main.example.ts
+++
@@ -1,5 +0,0 @@
-import { Path } from '../../Path.js';
-import { LoadIncludeFile, ProcessTemplateFile } from '../TemplateProcessor.js';
-
-await LoadIncludeFile('button', new Path('./component/button.html'));
-await ProcessTemplateFile(new Path('./index.template.html'), new Path('./index.html'));
