17f248292bda7009d87aadaa1137dc939b32782c
ericchase <github@eric.addymail.com>
2025-03-26 13:43:27 -0500

    extend globScan functionality
    Signed-off-by: ericchase <github@eric.addymail.com>

--- out/lib/ericchase/Platform/Glob_Utility.js
+++ out/lib/ericchase/Platform/Glob_Utility.js
@@ -14,14 +14,14 @@ export function globMatch(platform, query, include_patterns, exclude_patterns) {
   }
   return matched;
 }
-export async function globScan(platform, path, include_patterns, exclude_patterns) {
+export async function globScan(platform, path, include_patterns, exclude_patterns, absolutepaths = false, onlyfiles = true) {
   const included = [];
   for (const pattern of include_patterns) {
-    included.push(...await Array.fromAsync(platform.Directory.globScan(path, pattern)));
+    included.push(...await Array.fromAsync(platform.Directory.globScan(path, pattern, absolutepaths, onlyfiles)));
   }
   const excluded = [];
   for (const pattern of exclude_patterns) {
-    excluded.push(...await Array.fromAsync(platform.Directory.globScan(path, pattern)));
+    excluded.push(...await Array.fromAsync(platform.Directory.globScan(path, pattern, absolutepaths, onlyfiles)));
   }
   return new Set(included).difference(new Set(excluded));
 }
