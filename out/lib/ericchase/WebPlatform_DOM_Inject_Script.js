796c4f29659da5354e34a49b74b4d843dafb4e10
ericchase <github@eric.addymail.com>
2025-08-08 00:50:03 -0500

    might need to setup attributes before injecting code
    Signed-off-by: ericchase <github@eric.addymail.com>

--- out/lib/ericchase/WebPlatform_DOM_Inject_Script.js
+++ out/lib/ericchase/WebPlatform_DOM_Inject_Script.js
@@ -1,5 +1,6 @@
-export function WebPlatform_DOM_Inject_Script(code) {
+export function WebPlatform_DOM_Inject_Script(code, setup_fn) {
   const script = document.createElement("script");
+  setup_fn?.(script);
   script.textContent = code;
   document.body.appendChild(script);
   return script;
