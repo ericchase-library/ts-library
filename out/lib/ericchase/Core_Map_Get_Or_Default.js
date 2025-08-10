b46cbf97c0654316c259c6a8cb68bf29519c5253
ericchase <github@eric.addymail.com>
2025-08-10 01:32:47 -0500

    add async map get or default api
    Signed-off-by: ericchase <github@eric.addymail.com>

--- out/lib/ericchase/Core_Map_Get_Or_Default.js
+++ out/lib/ericchase/Core_Map_Get_Or_Default.js
@@ -6,3 +6,11 @@ export function Core_Map_Get_Or_Default(map, key, newValue) {
   map.set(key, value);
   return value;
 }
+export async function Async_Core_Map_Get_Or_Default(map, key, newValue) {
+  if (map.has(key)) {
+    return map.get(key);
+  }
+  const value = await newValue();
+  map.set(key, value);
+  return value;
+}
