1b32fa6bfc986260fa6b173257d917194b2f988d
ericchase <github@eric.addymail.com>
2025-08-18 01:52:40 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/projects-4-build.ts
+++ tools/projects-4-build.ts
@@ -3,11 +3,14 @@ import { SetLoggerOptions } from './core/Logger.js';
 import { Step_Async } from './core/step/Step_Async.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 
+const template_path = 'C:/Code/Base/JavaScript-TypeScript/@Template';
+
 import project_paths from './synced-projects.jsonc' assert { type: 'jsonc' };
 
 SetLoggerOptions({ ceremony: false });
 
 Builder.SetCleanUpSteps(
+  Step_Bun_Run({ cmd: ['bun', 'run', 'build'], dir: template_path, showlogs: false }),
   Step_Async(project_paths.map((path: string) => Step_Bun_Run({ cmd: ['bun', 'run', 'build'], dir: path, showlogs: true }))),
   //
 );
