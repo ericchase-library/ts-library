56fdce4eba32077c1f22df0b7e1127816d1bb7e5
ericchase <github@eric.addymail.com>
2025-08-20 02:28:56 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/core/bundle/semver-util/semver-util.d.ts
+++ tools/core/bundle/semver-util/semver-util.d.ts
@@ -5,6 +5,6 @@ interface Options {
 /** Base number to be used for the prerelease identifier */
 type IdentifierBase = '0' | '1';
 export declare namespace SEMVER_UTIL {
-  function increment(version: string, release: ReleaseType, options?: Options, identifier?: string, identifierBase?: IdentifierBase | false): string | null;
+  function increment(version: string, release: ReleaseType, options?: Options, identifier?: string, identifierBase?: IdentifierBase | false): string | undefined;
 }
 export {};
