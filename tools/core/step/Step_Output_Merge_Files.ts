50277ee24f7568999cb284b1ef872f839ebf6b0e
ericchase <github@eric.addymail.com>
2025-08-22 22:27:36 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/core/step/Step_Output_Merge_Files.ts
+++ tools/core/step/Step_Output_Merge_Files.ts
@@ -10,8 +10,8 @@ import { Logger } from '../../core/Logger.js';
 /**
  * This step processes files in `Builder.Dir.Out` only. Ensure that all config paths are relative to that folder.
  */
-export function Step_Output_Merge_Files(config: Config): Builder.Step {
-  return new Class(config);
+export function Step_Output_Merge_Files(...configs: Config[]): Builder.Step {
+  return new Class(configs);
 }
 
 class Class implements Builder.Step {
@@ -20,15 +20,15 @@ class Class implements Builder.Step {
 
   steps: Builder.Step[] = [];
 
-  constructor(readonly config: Config) {}
+  constructor(readonly configs: Config[]) {}
   async onRun(): Promise<void> {
-    for (const task of this.config.merge_tasks) {
-      switch (task.type) {
+    for (const config of this.configs) {
+      switch (config.type) {
         case 'json':
           {
             const data_list: any[] = [];
             // read merge files
-            for (const path of task.merge_files) {
+            for (const path of config.merge_files) {
               const { error, value } = await Async_BunPlatform_File_Read_Text(NODE_PATH.join(Builder.Dir.Out, path));
               if (value !== undefined) {
                 data_list.push(JSONC.parse(value));
@@ -37,23 +37,23 @@ class Class implements Builder.Step {
               }
             }
             const data = Core_JSON_Merge(...data_list);
-            this.channel.log(`Merge ${task.merge_files.map((_) => `"${_}"`).join(', ')}`);
-            await task.modify?.(data);
+            this.channel.log(`Merge ${config.merge_files.map((_) => `"${_}"`).join(', ')}`);
+            await config.modify?.(data);
             // delete merge files
-            for (const path of task.merge_files) {
+            for (const path of config.merge_files) {
               await Async_NodePlatform_File_Delete(path);
               this.channel.log(`Delete "${path}"`);
             }
             // write output file
-            await Async_BunPlatform_File_Write_Text(task.out_file, JSON.stringify(data, null, 2).trim() + '\n');
-            this.channel.log(`Write "${task.out_file}"`);
+            await Async_BunPlatform_File_Write_Text(config.out_file, JSON.stringify(data, null, 2).trim() + '\n');
+            this.channel.log(`Write "${config.out_file}"`);
           }
           break;
         case 'text':
           {
             const text_list: any[] = [];
             // read merge files
-            for (const path of task.merge_files) {
+            for (const path of config.merge_files) {
               const { error, value } = await Async_BunPlatform_File_Read_Text(NODE_PATH.join(Builder.Dir.Out, path));
               if (value !== undefined) {
                 text_list.push(value.trim());
@@ -62,41 +62,49 @@ class Class implements Builder.Step {
               }
             }
             const text = text_list.join('\n\n');
-            this.channel.log(`Merge ${task.merge_files.map((_) => `"${_}"`).join(', ')}`);
+            this.channel.log(`Merge ${config.merge_files.map((_) => `"${_}"`).join(', ')}`);
             // delete merge files
-            for (const path of task.merge_files) {
+            for (const path of config.merge_files) {
               await Async_NodePlatform_File_Delete(path);
               this.channel.log(`Delete "${path}"`);
             }
             // write output file
-            await Async_BunPlatform_File_Write_Text(task.out_file, text + '\n');
-            this.channel.log(`Write "${task.out_file}"`);
+            await Async_BunPlatform_File_Write_Text(config.out_file, text + '\n');
+            this.channel.log(`Write "${config.out_file}"`);
           }
           break;
       }
     }
   }
 }
-interface Config {
-  /**
-   * For `type` `json`, later files in `merge_list` merge onto earlier files.
-   *
-   * For `type` `text`, later files in `merge_list` append to bottom of output
-   * file.
-   *
-   * In all cases, `merge_files` are deleted, and `out_file` is overwritten.
-   */
-  merge_tasks: (
-    | {
-        type: 'json';
-        merge_files: string[];
-        out_file: string;
-        modify?: (data: any) => Promise<void> | void;
-      }
-    | {
-        type: 'text';
-        merge_files: string[];
-        out_file: string;
-      }
-  )[];
-}
+type Config =
+  | {
+      /**
+       * For `type` `json`, later files merge onto earlier files.
+       */
+      type: 'json';
+      /**
+       * Merged files are deleted on successful merging.
+       */
+      merge_files: string[];
+      /**
+       * Out file is overwritten on successful merging.
+       */
+      out_file: string;
+      modify?: (data: any) => Promise<void> | void;
+    }
+  | {
+      /**
+       * For `type` `text`, later files in `merge_list` append to bottom of
+       * output file.
+       */
+      type: 'text';
+      /**
+       * Merged files are deleted on successful merging.
+       */
+      merge_files: string[];
+      /**
+       * Out file is overwritten on successful merging.
+       */
+      out_file: string;
+    };
